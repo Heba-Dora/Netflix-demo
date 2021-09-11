5d9717704445f2c72a305bbdbecda7814849fc7f
Heba <hebamirdora@gmail.com>
2021-09-11 17:12:46 +0200

    netflix clone

--- src/Banner.js
+++ src/Banner.js
@@ -2,11 +2,14 @@ import React, {useState, useEffect} from 'react'
 import requests from './requests'
 import axios from './axios'
 import './Banner.css'
+import YouTube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
 
 
 function Banner() {
     const [movies, setMovies] = useState([])
+    const [trailerUrl, setTrailerUrl] = useState('')
 
 
     useEffect(()=>{
@@ -19,9 +22,30 @@ function Banner() {
         } 
         fetchData()
     }, [])
-    function truncate(str, n) {
-        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-      }
+
+    const opts ={
+        height:'390',
+        width:'100%',
+        playerVars: {
+            autoplay: 1,
+
+        }
+    }
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            movieTrailer(movie?.name || movie?.title || movie?.source)
+            .then((url)=> {
+                const urlParams = new URLSearchParams( new URL(url).search)
+                setTrailerUrl(urlParams.get('v'))
+
+            })
+            .catch((error)=> console.log(error))
+        }
+
+    }
     
     return (
         <header>
@@ -40,14 +64,17 @@ function Banner() {
          
                </div> 
                <div className="banner_buttons">
-               <button className="banner_button1">Play</button>
+               <button className="banner_button1" onClick={()=>handleClick(movies)} >Play </button>
                    <button className="banner_button2">More Info</button>
+                  
 
                </div>
             
            
-            
+               
         </div>
+        <div>{trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}</div>
+        
         </header>
        
     )
